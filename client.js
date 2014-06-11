7e7b0c34e8e49ecd1ed1deed84f18d42c21b2167
Stephen Jones <stephen.jones@whiteboard-it.com>
2014-06-10 19:56:32 -0500

    whitespace

--- client.js
+++ client.js
@@ -1,125 +1,28 @@
 #!/usr/bin/env node
+var request   = require('request')
+  , qs        = require('qs')
+  , randomBot = require('./bot');
 
-var request = require('request');
-var qs = require('qs');
-
-var randomBot = require('./bot');
-
-function start(serverUrl, key, mode, numTurns, bot, cb) {
-  var state;
-  if ('arena' === mode) {
-    console.log('Connected and waiting for other players to join...');
-  }
-  getNewGameState(serverUrl, key, mode, numTurns, function(err, state) {
-    if (err) {
-      console.log("ERROR starting game:", err);
-      return cb();
-    }
-    console.log('Playing at:', state['viewUrl']);
-
-    loop(key, state, bot, cb);
-  });
-}
-
-function getNewGameState(serverUrl, key, mode, numTurns, cb) {
-  var params = {
-    key: key
-  };
-  var apiEndpoint = '/api/arena';
-  if ('training' === mode) {
-    params.turns = numTurns;
-    // Comment out map parameter for a random map
-    params.map = 'm1';
-    apiEndpoint = '/api/training';
-  }
-  request.post({
-    url: serverUrl + apiEndpoint,
-    body: qs.stringify(params),
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-  }, function(err, res, body) {
-    if (err || 200 !== res.statusCode) {
-      cb(err || "Unable to start new game status code: " + res.statusCode +
-        " " + body);
-    } else {
-      cb(null, JSON.parse(new Buffer(body, 'utf8').toString('utf8')));
-    }
-  });
-}
-
-function loop(key, state, bot, cb) {
-
-  if (isFinished(state)) {
-    cb();
-  } else {
-    process.stdout.write('.');
-    var url = state['playUrl'];
-    bot(state, function(dir) {
-      state = move(url, key, dir, function(err, newState) {
-        if (err) {
-          console.log('ERROR:', err);
-          cb();
-        } else {
-          loop(key, newState, bot, cb);
-        }
-      });
-    });
-  }
-}
-
-function isFinished(state) {
-  return state && !! state.game &&
-    true === state['game']['finished'];
-}
-
-function move(url, key, direction, cb) {
-  request.post({
-    url: url,
-    body: qs.stringify({
-      key: key,
-      dir: direction
-    }),
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-  }, function(err, res, body) {
-    if (err || 200 !== res.statusCode) {
-      cb(err || 'Unable to move status code:' + res.statusCode + ' ' + body);
-    } else {
-      cb(null, JSON.parse(new Buffer(body, 'utf8').toString('utf8')));
-    }
-
-  });
-}
-
-function usage() {
-  console.log('Usage: client.js <key> <[training|arena]> <number-of-games|number-of-turns> [server-url]');
-  console.log('Example: client.js mySecretKey training 20');
-}
 
 var argv = process.argv;
-if (6 < argv.length) {
+if (argv.length < 5) {
   usage();
   process.exit(1);
 }
-var key = argv[2];
-var mode = argv[3];
 
-var numberOfGames = parseInt(argv[4], 10);
-var numberOfTurns = 300; // Ignored in arena mode
+var key = argv[2]
+  , mode = argv[3]
+  , numberOfGames = parseInt(argv[4], 10)
+  , numberOfTurns = 300
+  , serverUrl = argv[5] || 'http://vindinium.org'
+  , i = 0;
 
-if ('training' === mode) {
+if (mode === 'training') {
   numberOfGames = 1;
   numberOfTurns = parseInt(argv[4], 10);
 }
 
-var serverUrl = 'http://vindinium.org';
-if (6 === argv.length) {
-  serverUrl = argv[5];
-}
-
-var i = 0;
+playGame();
 
 function playGame() {
   start(serverUrl, key, mode, numberOfTurns, randomBot, function() {
@@ -131,4 +34,95 @@ function playGame() {
   });
 }
 
-playGame();
+function start(serverUrl, key, mode, numTurns, bot, cb) { 
+  var state;
+
+  if (mode === 'arena') {
+    console.log('Connected and waiting for other players to join...');
+  }
+
+  getNewGameState(serverUrl, key, mode, numTurns, function (err, state) {
+    if (err) {
+      console.log("ERROR starting game:", err);
+      return cb();
+    }
+
+    console.log('Playing at:', state['viewUrl']);
+
+    loop(key, state, bot, cb);
+  });
+}
+
+function getNewGameState (serverUrl, key, mode, numTurns, cb) {
+
+  var apiEndpoint = mode === 'training' ? '/api/training' : '/api/arena'
+    , params      = { key: key }
+    , req         = { 
+                      url: serverUrl + apiEndpoint,
+                      body: qs.stringify(params),
+                      headers: { 'Content-Type': 'application/x-www-form-urlencoded' } 
+                    };
+
+  if (mode === 'training') { params.turns = numTurns; }
+
+  request.post(req, function (err, res, body) {
+    if (err || res.statusCode !== 200 ) {
+      cb(err || "Unable to start new game status code: " + res.statusCode + " " + body);
+    } else {
+      cb(null, JSON.parse(new Buffer(body, 'utf8').toString('utf8')));
+    }
+  });
+
+}
+
+function loop (key, state, bot, cb) {
+  var url = state['playUrl'];
+
+  if (isFinished(state)) {
+    cb();
+  } 
+  else {
+    bot(state, function (dir) {
+      state = move(url, key, dir, function(err, newState) {
+        if (err) {
+          console.log('ERROR:', err);
+          cb();
+        } 
+        else {
+          loop(key, newState, bot, cb);
+        }
+      });
+    });
+  }
+}
+
+function isFinished (state) {
+  return   state 
+        && !!state.game 
+        && state['game']['finished'] === true;
+}
+
+function move (url, key, direction, cb) {
+  var req = {
+    url: url,
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: qs.stringify({ key: key, dir: direction })
+  };
+
+  request.post(req, function (err, res, body) {
+    if (err || 200 !== res.statusCode) {
+      cb(err || 'Unable to move status code:' + res.statusCode + ' ' + body);
+    } else {
+      cb(null, JSON.parse(new Buffer(body, 'utf8').toString('utf8')));
+    }
+  });
+}
+
+function usage () {
+  console.log('Usage: client.js <key> <[training|arena]> <number-of-games|number-of-turns> [server-url]');
+  console.log('Example: client.js mySecretKey training 20');
+}
+
+function board (board) {
+
+}
